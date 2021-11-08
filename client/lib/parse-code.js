12e66d981ec689897d1281f894f20e21e64fc730
jsygo <joshsygobusiness@gmail.com>
2021-11-08 04:46:59 +0000

    removed code comments from parse-code

--- client/lib/parse-code.js
+++ client/lib/parse-code.js
@@ -1,36 +1,4 @@
 const parseCode = (htmlInput, cssInput, javascriptInput) => {
-  // i need to check for <style>, <script>
-  // i need to go from the inside, out
-  // i dont think im gonna check fro these right now, just <script> and <style>
-  // the iframe should deal with the rest but i can deal with it if i run into a problem
-
-  // if (!htmlInput.includes('<style>')) {
-  //   htmlInput = `<style>\n${cssInput}\n</style>\n${htmlInput}`;
-  // } else {
-  //   const htmlComponents = htmlInput.split('<style>');
-  //   htmlComponents[2] = htmlComponents[1].split('</style>')[1];
-  //   htmlComponents[1] = htmlComponents[1].split('</style>')[0];
-  //   htmlComponents[1] = `<style>\n${cssInput}\n${htmlComponents[1]}\n</style>`;
-
-  //   htmlInput = '';
-  //   for (let i = 0; i < htmlComponents.length; i++) {
-  //     htmlInput += htmlComponents[i];
-  //   }
-  // }
-
-  // if (!htmlInput.includes('<script>')) {
-  //   htmlInput = `${htmlInput}\n<script>\n${javascriptInput}\n</script>`;
-  // } else {
-  //   const htmlComponents = htmlInput.split('<script>');
-  //   htmlComponents[2] = htmlComponents[1].split('</script>')[1];
-  //   htmlComponents[1] = htmlComponents[1].split('</script>')[0];
-  //   htmlComponents[1] = `<script>\n${javascriptInput}\n${htmlComponents[1]}\n</script>`;
-
-  //   htmlInput = '';
-  //   for (let i = 0; i < htmlComponents.length; i++) {
-  //     htmlInput += htmlComponents[i];
-  //   }
-  // }
   cssInput = `<style>${cssInput}</style>`;
   javascriptInput = `<script>${javascriptInput}</script>`;
 
