27bf733dc00116f6ba7cc5febb8b1234778cda4a
jsygo <joshsygobusiness@gmail.com>
2021-11-19 19:39:05 +0000

    removed test users and unused files

--- database/data.sql
+++ database/data.sql
@@ -1,5 +1,3 @@
 insert into "users" ("username", "password")
-             values ('testUser1', '$argon2i$v=19$m=4096,t=3,p=1$KcYLYDQRPx0zte/m4SsqCQ$jbo7UklwemiDavmzLOnMcmLGrmdImpB9mO/6nkacV6E'),
-                    ('testUser2', '$argon2i$v=19$m=4096,t=3,p=1$XAVazRbPtZeejj7a/AmDqQ$+BYKFgDGCnnrz0b5wNB8lEhuEmxn90bbyIonRE6LK0Q'),
-                    ('guest', '$argon2i$v=19$m=4096,t=3,p=1$EApciDleJ4+5F03XVuUQCg$ApYzgGZU6QBT8NCYiTb+zhapfDHb3yL/6Inx+x/U340'),
+             values ('guest', '$argon2i$v=19$m=4096,t=3,p=1$EApciDleJ4+5F03XVuUQCg$ApYzgGZU6QBT8NCYiTb+zhapfDHb3yL/6Inx+x/U340'),
                     ('jsygo', '$argon2i$v=19$m=4096,t=3,p=1$V91vhXIpAEe3g/9JIQKKMw$a42FL6sGy3irIyWwHoJxW5Li0K2+6FE+TdIvjPhnbec')
